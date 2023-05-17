367b4bb862e3dd311ed0bc0da416c9c7d4a6e493
ValeriaRodrigues-Dev <114243387+ValeriaRodrigues-Dev@users.noreply.github.com>
2023-05-17 11:28:26 -0300

    udemy002

---
+++ Udemy/Com Nodejs/ex002/index.js
@@ -0,0 +1,15 @@
+console.log('-----------------------------')
+console.log('MEDIDOR DE IMC')
+console.log('-----------------------------')
+
+const nome = 'Luidin Ortano'
+const sobrenome = 'Melandi'
+const idade = 43
+const peso = 84
+const altura = 1.80
+let imc = peso / (altura **2)
+let anoNascimento = 1980
+
+console.log(nome, sobrenome) 
+console.log(`Nasceu em: ${anoNascimento}, Possui: ${idade} anos, Pesa: ${peso} kg`)
+console.log(`Tem: ${altura} de altura e seu imc é de: ${imc}`);
